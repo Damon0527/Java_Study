d2fab8af4d2766823ece41b5c8eef502b33bb6e8
Damon0527 <damon@spreatics.com>
2024-10-10 17:50:55 +0900

    first commit

---
+++ Java/src/_05_class/Student.java
@@ -0,0 +1,18 @@
+package _05_class;
+
+// 클래스
+// - 객체지향 프로그래밍의 기초
+// - "클래스"를 통해 "인스턴스"를 생성
+
+// (공개) 클래스 선언
+public class Student {
+
+    // 필드
+    public String name;
+    public int grade;
+
+    // 생성자
+    // - 객체가 생성될 때 자동으로 호출되는 특별한 종류의 메서드
+    public Student(String name, int grade) {}
+
+}
---
+++ Java/src/_05_class/StudentEx.java
@@ -0,0 +1,7 @@
+package _05_class;
+
+public class StudentEx {
+    public static void main(String[] args) {
+
+    }
+}
